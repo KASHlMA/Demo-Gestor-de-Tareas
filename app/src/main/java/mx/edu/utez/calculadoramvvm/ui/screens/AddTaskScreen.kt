efd55df1f832795d311dfa192dc6612bbb26abb7
ShannyRamos17 <20243ds135@utez.edu.mx>
2025-10-27 22:06:19 -0600

    Agregamos texto, le cambiamos el tamaño a la letra y el tipo de letra

--- app/src/main/java/mx/edu/utez/calculadoramvvm/ui/screens/AddTaskScreen.kt
+++ app/src/main/java/mx/edu/utez/calculadoramvvm/ui/screens/AddTaskScreen.kt
@@ -2,7 +2,10 @@ package mx.edu.utez.calculadoramvvm.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,7 +13,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 
@@ -27,7 +32,21 @@ import androidx.navigation.NavController
                 .fillMaxSize()
                 .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){}
+        ){
+            Text(
+                text = "Agregar tarea",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Título de la tarea") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
 
     }
 
