51c29d82d3850d2ff9466bc281bda64806e1284d
ShannyRamos17 <20243ds135@utez.edu.mx>
2025-10-27 22:04:36 -0600

    Agregamos la columna con fillMaxSize y padding

--- app/src/main/java/mx/edu/utez/calculadoramvvm/ui/screens/AddTaskScreen.kt
+++ app/src/main/java/mx/edu/utez/calculadoramvvm/ui/screens/AddTaskScreen.kt
@@ -1,10 +1,16 @@
 package mx.edu.utez.calculadoramvvm.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 
@@ -14,4 +20,16 @@ import androidx.navigation.NavController
     fun AddTaskScreen(navController: NavController, viewModel: ViewModel){
         var title by remember { mutableStateOf("") }
         var description by remember { mutableStateOf("") }
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){}
+
     }
+
+
+
