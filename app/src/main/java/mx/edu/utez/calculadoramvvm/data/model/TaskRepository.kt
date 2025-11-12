4d2f2fee970ed9e9638526e1e2285ca7593fa592
KASHlMA <gustavoanayalopez32@gmail.com>
2025-11-11 23:21:43 -0600

    Se agrego un .xml para poder acceder a la red y se agrego un permiso en AndroidManifest.xml para poder acceder a internet

--- app/src/main/java/mx/edu/utez/calculadoramvvm/data/model/TaskRepository.kt
+++ app/src/main/java/mx/edu/utez/calculadoramvvm/data/model/TaskRepository.kt
@@ -1,14 +1,47 @@
 package mx.edu.utez.calculadoramvvm.data.model
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import mx.edu.utez.calculadoramvvm.data.network.ApiClient
+import mx.edu.utez.calculadoramvvm.data.network.TaskApiService
 
 class TaskRepository(private val taskDao: TaskDao) {
 
-    fun getAllTasks(): Flow<List<Task>> = taskDao.getAllTasks()
+    private val api = ApiClient.retrofit.create(TaskApiService::class.java)
 
-    suspend fun insertTask(task: Task) = taskDao.insertTask(task)
+    // Se obtienen las tareas desde el servidor
+    fun getAllTasks(): Flow<List<Task>> = flow {
+        val response = api.getTasks()
+        if (response.isSuccessful && !response.body().isNullOrEmpty()) {
+            emit(response.body()!!)
+        } else {
+            // Aqui se emite lo que hay en la base de datos local
+            taskDao.getAllTasks().collect { localList ->
+                emit(localList)
+            }
+        }
+    }
 
-    suspend fun updateTask(task: Task) = taskDao.updateTask(task)
 
-    suspend fun deleteTask(task: Task) = taskDao.deleteTask(task)
+
+    suspend fun insertTask(task: Task) {
+        val response = api.createTask(task)
+        if (response.isSuccessful) {
+            taskDao.insertTask(response.body()!!)
+        }
+    }
+
+    suspend fun updateTask(task: Task) {
+        val response = api.updateTask(task.id, task)
+        if (response.isSuccessful) {
+            taskDao.insertTask(response.body()!!)
+        }
+    }
+
+    suspend fun deleteTask(task: Task) {
+        val response = api.deleteTask(task.id)
+        if (response.isSuccessful) {
+            taskDao.deleteTask(task)
+        }
+    }
 }
