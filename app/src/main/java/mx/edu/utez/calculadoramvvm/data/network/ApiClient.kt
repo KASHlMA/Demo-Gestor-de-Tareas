e025c328d7fc00f8fd3a4cd1acf3f1999fb52ab8
KASHlMA <gustavoanayalopez32@gmail.com>
2025-12-11 18:59:52 -0600

    comment

--- app/src/main/java/mx/edu/utez/calculadoramvvm/data/network/ApiClient.kt
+++ app/src/main/java/mx/edu/utez/calculadoramvvm/data/network/ApiClient.kt
@@ -7,7 +7,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 object ApiClient {
 
     //Se ingresa la url de la api y se cambia aqui dependiendo de la red usando ipconfig para ver la red
-    private const val BASE_URL = "http://192.168.106.25:5000/"
+    private const val BASE_URL = "http://192.168.109.215:5000/"
 
     private val logger = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
